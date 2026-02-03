441d505e680d10b40596dc86f268b7bb48a1c72e
姜源 <yuan62387@qq.com>
2026-02-03 16:29:02 +0800

    首页列表：商品列表请求和渲染展示、列表滚动到底懒加载处理、下拉刷新逻辑

--- pages/home/home.js
+++ pages/home/home.js
@@ -8,7 +8,10 @@ Page({
    */
   data: {
     BASE_URL: '',
-    looplist: []
+    looplist: [],
+    goodlist: [],
+    currentPage: 1,
+    goodTotal: 0
   },
 
   /**
@@ -19,7 +22,8 @@ Page({
     this.setData({
       BASE_URL: getApp().globalData.BASE_URL
     });
-    this.renderSwiper()
+    this.renderSwiper() //轮播
+    this.renderGoods() //商品
   },
 
   renderSwiper() {
@@ -27,6 +31,19 @@ Page({
       console.log(res);
       this.setData({
         looplist: res
+      })
+    }).catch(err => {
+      console.error(err);
+    })
+  },
+
+  renderGoods() {
+    //默认分页10条，否则翻页不生效 json-server 1.0.0-beta.3
+    request({ url: `/goods?_page=${this.data.currentPage}` }).then(res => {
+      console.log(res.data); // _page 默认10条，数据在 data 中
+      this.setData({
+        goodlist: [...this.data.goodlist, ...res.data],
+        goodTotal: res.items
       })
     }).catch(err => {
       console.error(err);
@@ -65,14 +82,27 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh() {
-
+    setTimeout(() => {
+      //更新数据
+      console.log("下拉更新数据了");
+      wx.stopPullDownRefresh() //停止下拉刷新
+    }, 1000) //eg: 1s时间
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom() {
-
+    // 懒加载效果，滚动到底部按分页加载
+    console.log(this.data.goodlist.length, this.data.goodTotal);
+    if (this.data.goodlist?.length === this.data.goodTotal) {
+      console.log("滚动到底了...");
+      return
+    }
+    this.setData({
+      currentPage: this.data.currentPage + 1
+    })
+    this.renderGoods() // current 增加追加渲染列表数据
   },
 
   /**
