89fb4deb85f28679d74b7e1c01ca294758be0ab8
姜源 <yuan62387@qq.com>
2026-02-04 17:28:25 +0800

    个人中心：布局、换头像功能

--- pages/center/center.js
+++ pages/center/center.js
@@ -7,14 +7,44 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    BASE_URL: '',
+    userInfo: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    // 获取小程序全局实例，将全局基地址赋值到页面data
+    this.setData({
+      BASE_URL: getApp().globalData.BASE_URL
+    });
+  },
+  //更换头像：拍摄或从手机相册中选择图片或视频
+  handleAvatarChange() {
+    wx.chooseMedia({
+      count: 1, //选择图片数量
+      mediaType: ['image','video'],
+      sourceType: ['album', 'camera'],
+      maxDuration: 30,
+      camera: 'back',
+      //success(res) {
+      success: (res) => {  //使用箭头函数 确保 this 指向为当前页面对象
+        console.log(res.tempFiles[0].tempFilePath)
+        console.log(res.tempFiles[0].size)
+        this.setData({
+          userInfo: {
+            ...this.data.userInfo,
+            avatarUrl: res.tempFiles[0].tempFilePath
+          }
+        })
+        //放在本地存储中
+        wx.setStorageSync("token", {
+          ...wx.getStorageSync("token"),
+          avatarUrl: res.tempFiles[0].tempFilePath
+        })
+      }
+    })
   },
 
   /**
@@ -30,6 +60,9 @@ Page({
   onShow() {
     checkAuth(() => {
       console.log("进入我的");
+      this.setData({
+        userInfo: wx.getStorageSync("token")
+      })
     })
   },
 
